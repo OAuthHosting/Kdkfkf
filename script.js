80373d001cba6b63763fd86527a2b79b315edc9e
OAuthHosting <contact@oauth2hosting.spike.team>
2024-06-19 09:33:53 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('signup-button').addEventListener('click', function() {
+    document.getElementById('form-title').textContent = 'Sign Up';
+});
+
+document.getElementById('login-button').addEventListener('click', function() {
+    document.getElementById('form-title').textContent = 'Login';
+});
